429136080f3aab61a3ebcec6936158822c39f9b2
Avinash Joshi <avinash@hey.com>
2024-05-20 23:27:58 -0500

    add none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "vim", "javascript", "html", "ruby" },
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			-- ensure_installed = { "lua", "vim", "javascript", "html", "ruby" },
+			autoinstall = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
