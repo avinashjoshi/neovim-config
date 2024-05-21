febf64d4964e51336fe9115fdbc62fe792ee5731
Avinash Joshi <avinash@hey.com>
2024-05-21 10:24:36 -0500

    barbar for buffer tabs

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,21 @@
+return {
+  "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",   -- OPTIONAL: for git status
+    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+    -- Move to previous/next
+    map("n", "<cs-h>", "<Cmd>BufferPrevious<CR>", opts)
+    map("n", "<cs-l>", "<Cmd>BufferNext<CR>", opts)
+  end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    -- animation = true,
+    -- insert_at_start = true,
+    -- …etc.
+  },
+}
