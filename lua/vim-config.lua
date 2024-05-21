403e6484b3f2170f3c740bbfe09f9a9823304877
Avinash Joshi <avinash@hey.com>
2024-05-21 00:01:29 -0500

    navigate shortcuts

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -5,3 +5,12 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.wo.number = true
 
+vim.cmd("set colorcolumn=120")
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<cs-h>", ":tabprevious<CR>")
+vim.keymap.set("n", "<cs-l>", ":tabnext<CR>")
