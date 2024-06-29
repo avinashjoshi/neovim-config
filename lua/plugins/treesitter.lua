d8aad87d8022fe1f35a31edca0da01d136b52f18
Avinash Joshi <avinash@hey.com>
2024-06-29 09:01:09 -0500

    Updated a bunch of things and added some plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,32 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "RRethy/nvim-treesitter-endwise" },
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      -- ensure_installed = { "lua", "vim", "javascript", "html", "ruby" },
+      ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      endwise = { enable = true },
       autoinstall = true,
-      highlight = { enable = true },
+      highlight = { enable = true, disable = { "yaml", "ruby" } },
       indent = { enable = true },
     })
   end,
