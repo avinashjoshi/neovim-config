339db52b58358ee409186f53fe38ab8acf1af470
Avinash Joshi <avinash@cravd.com>
2025-08-09 15:03:31 -0700

    update packages and terminal shortcut

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Note: Scratch commands are now handled by the scratch-notes plugin
+-- You can use:
+-- :ScratchNew - Create a new note
+-- :ScratchNext - Switch to next note
+-- :ScratchPrev - Switch to previous note
+-- :ScratchList - Show list of notes
+--
+-- Or use the keymaps:
+-- <leader>sn - New scratch note
+-- <leader>sl - List scratch notes
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<leader>T", function()
+  vim.cmd("wincmd l")
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Open terminal" })
